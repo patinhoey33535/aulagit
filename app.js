215b7276ac61ef588ebf36596b2379f75232ce0a
pedro99771190 <pedro99771190@gmail.com>
2026-06-16 18:10:40 +0000

    pedro

--- app.js
+++ app.js
@@ -1,42 +1,13 @@
-module.exports = {soma:soma}
+const calculadora = require('./calculadora');
 
+let resultadoSoma = calculadora.soma(5,10);
+console.log(resultadoSoma);
 
+let resultadoSubtracao = calculadora.subtracao(10,5);
+console.log(resultadoSubtracao);
 
+let resultadoMultiplicacao = calculadora.multiplicacao(2,3);
+console.log(resultadoMultiplicacao);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let resultadoDivisao = calculadora.divisao(10,2);
+console.log(resultadoDivisao);  
